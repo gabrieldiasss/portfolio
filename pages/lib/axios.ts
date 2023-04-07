efc7b46516e631575d552756441b7ac7cecdbe52
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-04-07 11:08:04 -0300

    feat(api): implement project query endpoint

---
+++ pages/lib/axios.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.STRAPI_API_PUBLIC,
+  headers: { Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}` },
+});
